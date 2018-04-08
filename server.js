3dae906f05bde938e208bf242a3f59cb71d9a9da
JermySu <davis10733@hotmail.com>
2018-04-08 19:24:50 +0800

    Adds server.js

---
+++ server.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+const Koa = require('koa')
+const Router = require('koa-router')
+const helmet = require('koa-helmet')
+const logger = require('koa-logger')
+const cors = require('kcors')
+const paginator = require('koa-pagination')
+const bodyParser = require('koa-bodyparser')
+const config = require('./config')
+const routes = require('./routes')
+
+const fs = require('fs')
+
+const app = new Koa()
+
+require('./helpers')(app)
+
+app.use(helmet())
+app.use(logger())
+
+app.use(
+  paginator.middleware({
+    allowAll: false,
+    maximum: 25,
+  })
+)
+app.use(cors(config.cors))
+app.use(bodyParser(config.bodyParser))
+app.use(routes.routes())
+app.use(routes.allowedMethods())
+
+const router = new Router()
+
+router.get('*', async ctx => {
+  await handle(ctx.req, ctx.res)
+  ctx.respond = false
+})
+
+app.use(async (ctx, next) => {
+  ctx.res.statusCode = 200
+  await next()
+})
+
+app.use(router.routes())
+
+const server = app.listen(config.server.port, err => {
+  if (err) throw err
+  console.log(`Ready on http://localhost:${config.server.port}`)
+})
