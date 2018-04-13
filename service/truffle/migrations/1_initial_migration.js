47a89adf38d69a7e001d0afeb7153bd79858a942
JermySu <davis10733@hotmail.com>
2018-04-13 18:23:22 +0800

    Fixes the deploy script

--- service/truffle/migrations/1_initial_migration.js
+++ service/truffle/migrations/1_initial_migration.js
@@ -3,9 +3,10 @@ const truffleConfigFile = require('../truffle')
 var Migrations = artifacts.require("./Migrations.sol");
 
 module.exports = function(deployer, network) {
-  const truffleConfig = truffleConfigFile.networks[network]
-  const web3 = new Web3(new Web3.providers.HttpProvider(`http://${truffleConfig.host}:${truffleConfig.port}`))
-  web3.personal.unlockAccount(truffleConfig.from, truffleConfig.password, 180000)
-
+  if (network !== 'test') {
+    const truffleConfig = truffleConfigFile.networks[network]
+    const web3 = new Web3(new Web3.providers.HttpProvider(`http://${truffleConfig.host}:${truffleConfig.port}`))
+    web3.personal.unlockAccount(truffleConfig.from, truffleConfig.password, 180000)
+  }
   deployer.deploy(Migrations);
 };
