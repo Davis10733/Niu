2e7deade0cc8a6e04a8c71ae27ff6f503446c5db
JermySu <davis10733@hotmail.com>
2018-04-13 15:22:08 +0800

    Adds ethereum configs

--- config/index.js
+++ config/index.js
@@ -7,10 +7,17 @@ module.exports = {
   ipfs: {
     host: _.defaultTo(process.env.IPFS_HOST, 'localhost'),
     port: normalizePort(_.defaultTo(process.env.IPFS_PORT, 3000)),
+  },
+  ethereum: {
+    host: _.defaultTo(process.env.ETHEREUM_HOST, 'localhost'),
+    port: _.defaultTo(process.env.ETHEREUM_PORT, '8546'),
+    address: process.env.ETHEREUM_ADDRESS,
+    password: _.defaultTo(process.env.ETHEREUM_PASSWORD, '1234'),
+    contract: {
+      address: process.env.ETHEREUM_CONTRACT_ADDRESS,
+    }
   }
 }
-
-
 
 function normalizePort(val) {
   var port = parseInt(val, 10)
