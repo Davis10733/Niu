ae96acae3727a23e1a6b3f7f7bbc63d897e242ad
JermySu <davis10733@hotmail.com>
2018-04-14 13:03:37 +0800

    Adds development truffle config

--- service/truffle/truffle.js
+++ service/truffle/truffle.js
@@ -1,4 +1,11 @@
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+      from: '0x9b971ed37b62f46e0b83f111c9fadc7e72481f25',
+      password: '1234'
+    } 
+  }
 };
