331f2ed15452ae588d63f4b9a147afe017cd9a60
JermySu <davis10733@hotmail.com>
2018-04-14 13:00:13 +0800

    Adds user api into route

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ const router = new Router()
 const api = new Router()
 
 const post = require('./post-router')
+const user = require('./user-router')
 
 api.use(post)
+api.use(user)
+
 router.use('/api/v0', api.routes())
 
 module.exports = router
