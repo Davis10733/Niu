6f75680ec343f09f2ec760f5877107efe5e63268
JermySu <davis10733@hotmail.com>
2018-04-25 13:37:40 +0800

    Adds mail helper

---
+++ helpers/mail-helper.js
@@ -0,0 +1,37 @@
+const config = require('../config')
+const mailjet = require ('node-mailjet')
+  .connect(config.mail.publicKey, config.mail.privateKey)
+
+const getMessage = (userObject) => {
+  return {
+    'From': {
+      'Email': 'davis10733@hotmail.com',
+      'Name': 'INSIDER'
+    },
+    'To': [
+      {
+        'Email': userObject.email,
+        'Name': userObject.username,
+      }
+    ],
+    "TemplateID": 369034,
+    "TemplateLanguage": true,
+    "Subject": "Welcome to INSIDER",
+    "Variables": {
+       "confirmation_link": `${config.server.host}/active?address=${userObject.address}`
+    }
+  }
+}
+module.exports = {
+  sendActiveMail: async(userObject) => {
+    const message = getMessage(userObject)
+    return mailjet
+      .post('send', { 'version': 'v3.1' })
+      .request({
+        'Messages': [
+          message
+        ]
+      })
+  
+  }
+}
