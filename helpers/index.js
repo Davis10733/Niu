843ae8bd61db5de320219f902d709fff511b9fb3
JermySu <davis10733@hotmail.com>
2018-04-25 13:37:23 +0800

    Adds user and mail helpers into index

--- helpers/index.js
+++ helpers/index.js
@@ -1,9 +1,13 @@
 const ipfs = require('./ipfs-helper')
 const ethereum = require('./ethereum-helper')
+const user = require('./user-helper')
+const mail = require('./mail-helper')
 
 module.exports = (app) => {
   app.helpers = {
     ipfs,
     ethereum,
+    user,
+    mail,
   }
 }
