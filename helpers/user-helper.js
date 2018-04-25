7a1453cd88fc24cba57311a8b140ddf837580726
JermySu <davis10733@hotmail.com>
2018-04-25 13:38:01 +0800

    Adds user helper to create user object

---
+++ helpers/user-helper.js
@@ -0,0 +1,23 @@
+const keythereum = require('keythereum')
+const bcrypt = require('bcrypt')
+
+module.exports = {
+  createUserObject: async(ctx) => {
+    
+    // hash the password
+    const hashPw = await bcrypt.hash(ctx.request.body.password, 10)
+
+    // create key
+    let dk = keythereum.create()
+    let keyObject = keythereum.dump(hashPw, dk.privateKey, dk.salt, dk.iv)
+
+    return {
+      username: ctx.request.body.username,
+      password: hashPw,
+      address: keyObject.address, 
+      keyObject: keyObject,
+      email: ctx.request.body.email,
+      active: false,
+    }
+  }
+}
