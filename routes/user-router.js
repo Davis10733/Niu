60e6aaa66228612876365a97bc75feaf5bb74335
JermySu <davis10733@hotmail.com>
2018-04-26 11:21:21 +0800

    Adds user active API

--- routes/user-router.js
+++ routes/user-router.js
@@ -3,5 +3,6 @@ const ctrl = require('../controllers').user
 const router = new Router()
 
 router.post('/user', ctrl.create)
+router.post('/user/active', ctrl.active)
 
 module.exports = router.routes()
