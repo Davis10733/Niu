b0aac137bf25da6e9aabf57fa5fff5882e38f5b8
JermySu <davis10733@hotmail.com>
2018-04-27 15:28:37 +0800

    Adds insiderManager test case

--- service/truffle/test/insiderManager.js
+++ service/truffle/test/insiderManager.js
@@ -1,36 +1,36 @@
-const WriterManager = artifacts.require('WriterManager')
+const InsiderManager = artifacts.require('InsiderManager')
 const Writer = artifacts.require('Writer')
 const Base58 = require('base-58')
 
-contract('WriterManager', (accounts) => {
-  let writerManager
+contract('InsiderManager', (accounts) => {
+  let insiderManager
   const ipfsHash = 'QmNXnCWPS2szLaQGVA6TFtiUAJB2YnFTJJFTXPGuc4wocQ'
   
   beforeEach(async () => {
-    writerManager = await WriterManager.new({
+    insiderManager = await InsiderManager.new({
       from: accounts[0],
     })
   })
 
   it('should have an owner', async () => {
-    const owner = await writerManager.owner()
+    const owner = await insiderManager.owner()
     assert.isTrue(owner !== 0)
   })
 
   it('it is allowed to create writer', async () => {
-    await writerManager.createNewWriter(accounts[1], { from: accounts[1] })
-    const result = await writerManager.isWriter(accounts[1], { from: accounts[1] })
+    await insiderManager.createNewWriter(accounts[1], { from: accounts[1] })
+    const result = await insiderManager.isWriter(accounts[1], { from: accounts[1] })
     assert.isTrue(result)
   })
 
   it('is writer function is workable', async() => {
-    const result = await writerManager.isWriter(accounts[1], { from: accounts[1] })
+    const result = await insiderManager.isWriter(accounts[1], { from: accounts[1] })
     assert.isFalse(result)
   })
 
-  it('throw exception if we try to add article to invalid writer', async() => {
+  it('throw exception if we try to add article from invalid writer', async() => {
     try {
-      await writerManager.createNewPost(ipfsHash, { from: accounts[1] })
+      await insiderManager.createNewPost(ipfsHash, { from: accounts[1] })
       assert.fail('Should throw an error')
     } catch (error) {
       assert.isAbove(
@@ -41,10 +41,10 @@ contract('WriterManager', (accounts) => {
   })
 
   it('it is allowed to add article', async() => {
-    await writerManager.createNewWriter(accounts[1], { from: accounts[1] })
+    await insiderManager.createNewWriter(accounts[1], { from: accounts[1] })
     const unit8Array = fromIPFShash(ipfsHash)
     const hex = Buffer.from(unit8Array).toString('hex')
-    const transaction = await writerManager.createNewPost(`0x${hex}`, { from: accounts[1] })
+    const transaction = await insiderManager.createNewPost(`0x${hex}`, { from: accounts[1] })
     assert.isTrue(transaction.logs[0].event === 'NewPostCreated')
     
     const recoverHash = toIPFShash(transaction.logs[0].args.ipfsHash)
@@ -52,14 +52,27 @@ contract('WriterManager', (accounts) => {
   })
 
   it('it is allowed to add article by manager', async() => {
-    await writerManager.createNewWriter(accounts[1], { from: accounts[0] })
+    await insiderManager.createNewWriter(accounts[1], { from: accounts[0] })
     const unit8Array = fromIPFShash(ipfsHash)
     const hex = Buffer.from(unit8Array).toString('hex')
-    const transaction = await writerManager.createNewPostByManager(`0x${hex}`, accounts[1],{ from: accounts[0] })
+    const transaction = await insiderManager.createNewPostByManager(`0x${hex}`, accounts[1],{ from: accounts[0] })
     assert.isTrue(transaction.logs[0].event === 'NewPostCreated')
     
     const recoverHash = toIPFShash(transaction.logs[0].args.ipfsHash)
     assert.equal(recoverHash, ipfsHash)
+  })
+
+  it('isValidArticle function is workable', async() => {
+    await insiderManager.createNewWriter(accounts[1], { from: accounts[0] })
+    const unit8Array = fromIPFShash(ipfsHash)
+    const hex = Buffer.from(unit8Array).toString('hex')
+    await insiderManager.createNewPostByManager(`0x${hex}`, accounts[1],{ from: accounts[0] })
+    
+    let result = await insiderManager.isValidArticle(`0x${hex}`)
+    assert.isTrue(result)
+
+    result = await insiderManager.isValidArticle(`0x34222`)
+    assert.isFalse(result)
   })
 
   const fromIPFShash = (hash) => {
