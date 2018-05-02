91d15480bf84c904beb9155b6b187f101490300b
JermySu <davis10733@hotmail.com>
2018-05-02 14:35:47 +0800

    Adds jwt token generate function and login function

--- helpers/user-helper.js
+++ helpers/user-helper.js
@@ -1,5 +1,7 @@
 const keythereum = require('keythereum')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const config = require('../config')
 
 module.exports = {
   createUserObject: async(ctx) => {
@@ -9,7 +11,7 @@ module.exports = {
 
     // create key
     let dk = keythereum.create()
-    let keyObject = keythereum.dump(ctx.request.body.password, dk.privateKey, dk.salt, dk.iv)
+    let keyObject = keythereum.dump(hashPw, dk.privateKey, dk.salt, dk.iv)
 
     return {
       username: ctx.request.body.username,
@@ -18,5 +20,29 @@ module.exports = {
       keyObject: keyObject,
       email: ctx.request.body.email,
     }
+  },
+
+  login: async(ctx) => {
+    let user = await ctx.app.db.users.findByEmail(ctx.request.body.email)
+
+    if (user == undefined) {
+      throw new Error('User not found')
+    }
+
+    let result = await bcrypt.compare(ctx.request.body.password, user.password)
+
+    if (result == false) {
+      throw new Error('Invlaid password')
+    }
+
+    return user.toJSON()
+  },
+  
+  createJwt: async(user) => {
+    return jwt.sign({
+      'userId': user.id,
+      'address': user.address,
+      'exp': Math.floor(Date.now() / 1000) + (60 * 120)
+    }, config.jwt.secret)
   }
 }
