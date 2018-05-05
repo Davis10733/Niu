a3cebc3c38180e98917f668cee80a4807da6d933
JermySu <davis10733@hotmail.com>
2018-05-05 16:37:56 +0800

    Adds auth middleware

--- routes/post-router.js
+++ routes/post-router.js
@@ -1,7 +1,10 @@
 const Router = require('koa-router')
 const ctrl = require('../controllers').post
 const router = new Router()
+const config = require('../config')
+const { middleware } = require('@jermysu/koa-auth-middleware')
 
+router.use('/post/', middleware(config.jwt.secret))
 router.get('/post', ctrl.get)
 router.post('/post', ctrl.post)
 
