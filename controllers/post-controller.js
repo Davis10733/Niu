2462a5bb0c4690d42399336fc35e664db1485b95
JermySu <davis10733@hotmail.com>
2018-05-05 16:36:36 +0800

    Modifies create post method

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -3,17 +3,40 @@ const fs = require('fs')
 module.exports = {
   async post(ctx) {
     try {
-      const files = await ctx.app.helpers.ipfs.add([{
-        content: Buffer.from(JSON.stringify(ctx.request.body))
-      }])
+      await ctx.app.schemas.post.create(ctx.request.body)
+        .catch((e) => {
+          ctx.throw(400, e.message)
+        })
 
+      let user = await ctx.app.db.users.findByAddress(ctx.request.auth.address)
+
+      let post = {
+        ...ctx.request.body,
+        author: user.username,
+        publicKey: user.address
+      }
+
+      // Sending data into IPFS
+      const files = await ctx.app.helpers.ipfs.add([{
+        content: Buffer.from(JSON.stringify(post))
+      }])
       const ipfsHash = files[0].hash
-      
-      const etherumEvents = await ctx.app.helpers.ethereum.createNewPost(ctx, ipfsHash, ctx.request.body.writer)
+
+      // Add record into ethereum
+      const etherumEvents = await ctx.app.helpers.ethereum.createNewPost(ctx, ipfsHash, user.address)
+
+      // Add index in mysql
+      const postModel = await ctx.app.db.posts.createNewPost({
+        title: post.title,
+        ipfs_hash: ipfsHash,
+        tags: post.tags,
+      })
 
       ctx.body = {
+        post: postModel,
         success: true
       }
+
     } catch (e) {
       console.log(e)
       ctx.throw(e.status, e.message)
