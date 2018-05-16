d91c23faa89728e523552214b5509b89c92ad763
JermySu <davis10733@hotmail.com>
2018-05-16 11:06:20 +0800

    Gets IPFS content

--- helpers/ipfs-helper.js
+++ helpers/ipfs-helper.js
@@ -2,4 +2,26 @@ const config =  require('../config')
 const ipfsAPI = require('ipfs-api')
 const ipfs = ipfsAPI(config.ipfs.host, config.ipfs.port, {protocol: 'http'})
 
+ipfs.getContent = async(items) => {
+  if (!(items instanceof Array)) {
+    let content = await ipfs.files.cat(`/ipfs/${items.ipfs_hash}`)
+    content = JSON.parse(content.toString())
+    return {
+      ...items,
+      ...content,
+    }
+  }
+
+  items = items.map(async(item) => {
+    let content = await ipfs.files.cat(`/ipfs/${item.ipfs_hash}`)
+    content = JSON.parse(content.toString())
+
+    return {
+      ...item,
+      ...content,
+    }
+  })
+
+  return items
+}
 module.exports = ipfs
