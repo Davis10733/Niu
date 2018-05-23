c88d981add0bacb886cb128595e315682b866526
JermySu <davis10733@hotmail.com>
2018-05-23 15:50:11 +0800

    Refactors create post API for oribit db

--- controllers/post-controller.js
+++ controllers/post-controller.js
@@ -8,7 +8,8 @@ module.exports = {
           ctx.throw(400, e.message)
         })
 
-      let user = await ctx.app.db.User.findByAddress(ctx.request.auth.address)
+      let user = await ctx.app.helpers.redis.client.getAsync(ctx.request.auth.email)
+      user = JSON.parse(user)
 
       let post = {
         ...ctx.request.body,
@@ -16,24 +17,16 @@ module.exports = {
         publicKey: user.address
       }
 
-      // Sending data into IPFS
-      const files = await ctx.app.helpers.ipfs.add([{
-        content: Buffer.from(JSON.stringify(post))
-      }])
-      const ipfsHash = files[0].hash
+      // Saving data
+      let doc = await ctx.app.db.Post.createNewPost(post)
 
       // Add record into ethereum
-      const etherumEvents = await ctx.app.helpers.ethereum.createNewPost(ctx, ipfsHash, user.address)
-
-      // Add index in mysql
-      const postModel = await ctx.app.db.Post.createNewPost({
-        title: post.title,
-        ipfs_hash: ipfsHash,
-        tags: post.tags,
-      })
+      const etherumEvents = await ctx.app.helpers.ethereum.createNewPost(ctx, doc.ipfsHash, user.address)
 
       ctx.body = {
-        post: postModel,
+        post: {
+          ...doc,
+        },
         success: true
       }
 
