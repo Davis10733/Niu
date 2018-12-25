67b2de4dedbb2cb803d32d4fc10b36bc0088457f
JermySu <davis10733@hotmail.com>
2018-12-25 18:30:10 +0800

    Removes non use helper

--- helpers/index.js
+++ helpers/index.js
@@ -1,15 +1,11 @@
 const ipfs = require('./ipfs-helper')
-const ethereum = require('./ethereum-helper')
 const user = require('./user-helper')
-const mail = require('./mail-helper')
 const redis = require('./redis-helper')
 
-module.exports = (app) => {
+module.exports = app => {
   app.helpers = {
     ipfs,
-    ethereum,
     user,
-    mail,
-    redis,
+    redis
   }
 }
